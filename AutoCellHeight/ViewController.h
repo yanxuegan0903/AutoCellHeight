fa8ccb341493c7446a00a96b6b3202c1e321b830
yanxuegan <yanxuegan0903@163.com>
2017-04-01 13:53:15 +0800

    Initial Commit

---
+++ AutoCellHeight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoCellHeight
+//
+//  Created by vsKing on 2017/4/1.
+//  Copyright © 2017年 vsKing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AutoCellHeight/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AutoCellHeight
+//
+//  Created by vsKing on 2017/4/1.
+//  Copyright © 2017年 vsKing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
